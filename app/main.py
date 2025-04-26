83181658da955f0ce67fa67bdd8cd904e8d5660b
Kostya Mileant <kostya.milya@gmail.com>
2025-04-26 13:29:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -30,10 +30,8 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name, weight, coords=None):
         if coords is None:
             coords = [0, 0, 0]
-
-            if len(coords) == 2:
-
-                coords.append(0)
+        elif len(coords) == 2:
+            coords.append(0)
         super().__init__(name, weight, coords)
 
 
